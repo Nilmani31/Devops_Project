c4c5fbcff121fc17e8f866b59870421b4e982f41
Nilmani31 <hewamarambagechamsha@gmail.com>
2026-01-27 17:31:22 +0530

    main.tf changed again

--- main.tf
+++ main.tf
@@ -1,35 +1,31 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
 resource "null_resource" "deploy_app" {
-
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update -y",
-      "sudo apt install docker.io -y",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo usermod -aG docker ubuntu",
+      <<EOF
+sudo apt update -y
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
 
-      # Stop old containers
-      "docker stop frontend_c backend_c mongo_c || true",
-      "docker rm frontend_c backend_c mongo_c || true",
+# Stop old containers
+docker stop frontend_c backend_c mongo_c || true
+docker rm frontend_c backend_c mongo_c || true
 
-      # MongoDB
-      "docker run -d --name mongo_c -p 27017:27017 mongo:6.0",
+# MongoDB
+docker run -d --name mongo_c -p 27017:27017 mongo:6.0
 
-      # Backend
-      "docker run -d --name backend_c -p 4000:4000 \
-        -e MONGO_URI=mongodb://mongo_c:27017/idea_app \
-        -e SESSION_SECRET=Chamsha@31# \
-        --link mongo_c:mongo \
-        chamsha123/project-backend:latest",
+# Backend
+docker run -d --name backend_c -p 4000:4000 \
+  -e MONGO_URI="mongodb://mongo_c:27017/idea_app" \
+  -e SESSION_SECRET="Chamsha@31#" \
+  chamsha123/project-backend:latest
 
-      # Frontend
-      "docker run -d --name frontend_c -p 3000:3000 \
-        --link backend_c:backend \
-        chamsha123/project-frontend:latest"
+# Frontend
+docker run -d --name frontend_c -p 3000:3000 \
+  --link backend_c:backend \
+  chamsha123/project-frontend:latest
+EOF
     ]
 
     connection {
@@ -38,5 +34,10 @@ resource "null_resource" "deploy_app" {
       private_key = file("/var/lib/jenkins/.ssh/kserver.pem")
       host        = "3.108.2.120"
     }
+  }
+
+  triggers = {
+    backend_image  = "chamsha123/project-backend:latest"
+    frontend_image = "chamsha123/project-frontend:latest"
   }
 }
