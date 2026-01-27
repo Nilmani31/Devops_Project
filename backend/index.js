23d7ea94e9bd392d8283c3bdba9a14552f8e0609
Nilmani31 <hewamarambagechamsha@gmail.com>
2026-01-27 16:12:22 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,7 @@ const app = express();
 const port = 4000;
 app.use(express.json());
 
-app.use(cors({
-  origin: "http://localhost:3000", 
-  credentials: true
-}));  
+app.use(cors());  
 
 mongoose
   .connect(process.env.MONGO_URI, {
