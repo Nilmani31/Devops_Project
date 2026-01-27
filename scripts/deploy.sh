b78acec96bcab87edb2e15d7a3d3b0a997ebade6
Nilmani31 <hewamarambagechamsha@gmail.com>
2026-01-27 16:30:33 +0530

    change again deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+EC2_IP="3.108.2.120"  # replace with your EC2 public IP
+
 echo "Stopping existing containers..."
 docker stop frontend_c || true
 docker rm frontend_c || true
@@ -10,7 +12,7 @@ docker stop mongo_c || true
 docker rm mongo_c || true
 
 echo "Deploying MongoDB container..."
-docker run -d -p 27017:27017 --name  mongo_c mongo:6.0
+docker run -d -p 27017:27017 --name mongo_c mongo:6.0
 
 echo "Deploying backend container..."
 docker run -d -p 4000:4000 --name backend_c \
@@ -19,10 +21,9 @@ docker run -d -p 4000:4000 --name backend_c \
   -e SESSION_SECRET="Chamsha@31#" \
   chamsha123/project-backend:latest
 
-
 echo "Deploying frontend container..."
 docker run -d -p 3000:3000 --name frontend_c \
-  --link backend_c:backend \
+  -e REACT_APP_API_URL="http://$EC2_IP:4000/api" \
   chamsha123/project-frontend:latest
 
 echo "Deployment complete. Running containers:"
