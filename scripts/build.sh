6f53e48d7eff92db23609adadc97f1e1524dd4af
Nilmani31 <hewamarambagechamsha@gmail.com>
2026-01-27 15:21:25 +0530

    change build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Building updated frontend Docker image..."
-docker build -t Chamsha123/chamsha123/project-frontend ./frontend
-
-echo "Building updated backend Docker image..."
-docker build -t Chamsha123/chamsha123/project-backend ./backend
-
-echo "Build completed!"
+# Frontend
+docker build -t chamsha123/chamsha123/project-frontend:latest ./frontend
+# Backend
+docker build -t chamsha123/chamsha123/project-backend:latest ./backend
+echo "Build completed successfully!"
