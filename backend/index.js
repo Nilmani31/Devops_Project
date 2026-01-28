58645539003803e724f28e5525bcb69668546007
Nilmani31 <hewamarambagechamsha@gmail.com>
2026-01-28 21:37:50 +0530

    add dockergitignore file

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,12 @@ const app = express();
 const port = 4000;
 app.use(express.json());
 
-app.use(cors());  
+app.use(cors(
+  /*{origin: "http://localhost:3000",  
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true 
+  }*/
+));  
 
 mongoose
   .connect(process.env.MONGO_URI, {
